c3c49766cb539b2dc617057ed50277ea6575e5a8
Paul-Rajeevan_Nimalarajah <paulrajeevannimal@gmail.com>
2025-12-14 17:17:44 +0530

    generate random numbers

--- app/src/main/java/com/sldevelopers/findmax/MainActivity.kt
+++ app/src/main/java/com/sldevelopers/findmax/MainActivity.kt
@@ -9,25 +9,34 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var leftButton: Button
+    private lateinit var rightButton: Button
+    private lateinit var main: ConstraintLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val leftbutton = findViewById<Button>(R.id.leftbutton)
-        val rightbutton = findViewById<Button>(R.id.rightbutton)
-        val main = findViewById<ConstraintLayout>(R.id.main)
+        leftButton = findViewById(R.id.leftbutton)
+        rightButton = findViewById(R.id.rightbutton)
+        main = findViewById(R.id.main)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        leftbutton.setOnClickListener {
-            val leftNum = leftbutton.text.toString().toInt()
-            val rightNum = rightbutton.text.toString().toInt()
+
+        assignNumbersToButtons()
+
+        leftButton.setOnClickListener {
+            val leftNum = leftButton.text.toString().toInt()
+            val rightNum = rightButton.text.toString().toInt()
             if (leftNum > rightNum) {
                 main.setBackgroundColor(Color.parseColor("#90EE90"))
                 Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show()
@@ -35,10 +44,12 @@ class MainActivity : AppCompatActivity() {
                 main.setBackgroundColor(Color.parseColor("#F08080"))
                 Toast.makeText(this, "Wrong!", Toast.LENGTH_SHORT).show()
             }
+            assignNumbersToButtons()
         }
-        rightbutton.setOnClickListener {
-            val leftNum = leftbutton.text.toString().toInt()
-            val rightNum = rightbutton.text.toString().toInt()
+
+        rightButton.setOnClickListener {
+            val leftNum = leftButton.text.toString().toInt()
+            val rightNum = rightButton.text.toString().toInt()
             if (rightNum > leftNum) {
                 main.setBackgroundColor(Color.parseColor("#90EE90"))
                 Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show()
@@ -46,7 +57,17 @@ class MainActivity : AppCompatActivity() {
                 main.setBackgroundColor(Color.parseColor("#F08080"))
                 Toast.makeText(this, "Wrong!", Toast.LENGTH_SHORT).show()
             }
+            assignNumbersToButtons()
         }
+    }
 
+    private fun assignNumbersToButtons() {
+        val leftNum = Random.nextInt(100)
+        var rightNum = Random.nextInt(100)
+        while (rightNum == leftNum) {
+            rightNum = Random.nextInt(100)
+        }
+        leftButton.text = leftNum.toString()
+        rightButton.text = rightNum.toString()
     }
 }
